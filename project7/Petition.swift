8edd4b3840be1f8cf5796239be3e1d29de7baafa
godgiftedboy <rajprajapati12321@gmail.com>
2025-04-08 13:29:41 +0545

    chore: parsed json and displayed it in a table view

---
+++ project7/Petition.swift
@@ -0,0 +1,13 @@
+//
+//  Petition.swift
+//  project7
+//
+//  Created by Waterflow Technology on 08/04/2025.
+//
+
+import Foundation
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ project7/Petitions.swift
@@ -0,0 +1,11 @@
+//
+//  Petitions.swift
+//  project7
+//
+//  Created by Waterflow Technology on 08/04/2025.
+//
+
+import Foundation
+struct Petitions: Codable {
+    var results: [Petition]
+}
